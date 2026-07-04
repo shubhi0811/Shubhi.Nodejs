fabac194f0a0560caa27932ad7251caa08bc343b
Shubhi Saxena <shubhisaxena0811@gmail.com>
2026-07-04 09:56:47 +0530

    random user toggle

--- frontend/index.js
+++ frontend/index.js
@@ -1,31 +1,51 @@
-
-
-console.log("Hello from JS");
-//alert("Hello from JS");
 const users = [
   {
-    "name" :"Jane Doe",
+    "name" : "Jane Doe",
     "gender" : "Female",
-    "image" : "../images/jane.png"
+    "image" : "./images/jane.png"
   },
   {
-    "name" :"John Doe",
+    "name" : "John Doe",
     "gender" : "Male",
-    "image" : "../images/john.png"
+    "image" : "./images/john.png"
   },
 ]
+
 let curUserId = 0;
+
 function toggle(){
   if(curUserId == 0)
-    curUserId=1;
+    curUserId = 1;
   else
-    curUserId=0;
+    curUserId = 0;
+
   // image
   document.getElementById("img").src = users[curUserId].image;
   // name
   document.getElementById("card-name").innerText = users[curUserId].name;
-
   // gender
   document.getElementById("card-gender").innerText = users[curUserId].gender;
+}
 
-} 
+function getRandomUser(){
+  fetch("https://randomuser.me/api/")
+  .then(function(data) {
+    return data.json();
+  })
+  .then(function(parsedData) {
+    // Handle the fetched data
+    //console.log(parsedData);
+    let gender = parsedData.results[0].gender;
+    let first = parsedData.results[0].name.first;
+    let last = parsedData.results[0].name.last;
+    let name = first + " " + last;
+    let imageUrl = parsedData.results[0].picture.large;
+
+    //gender
+    document.getElementById("card-gender").innerText = gender;
+    //name
+    document.getElementById("card-name").innerText = name;
+    //image
+    document.getElementById("img").src = imageUrl;
+  });
+}
